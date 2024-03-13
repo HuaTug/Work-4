c8ad72e3c3afc8d63ef982eb1ac3cb15f5e16c7d
Diligenthader <3152223434@qq.com>
2024-03-13 23:49:50 +0800

    PR

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"4-TikTok/IM"
 	"4-TikTok/configs"
 	"4-TikTok/router"
+	"fmt"
 )
 
 func main() {
 	configs.Init()
 	go IM.Manager.Start()
 	r := router.NewRouter()
+	fmt.Println("这是一次PR尝试")
 	_ = r.Run(":10001")
 }
